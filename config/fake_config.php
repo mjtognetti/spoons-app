7603de6eb1b987f8a9d1aac96cc84ab7db4f2373
Matt Tognetti <mjtognetti@gmail.com>
2012-02-10 21:22:58 -0800

    fake config file added

---
+++ config/fake_config.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../app/lib/meekrodb.2.0.class.php';
+
+DB::$user = 'user';
+DB::$password = 'password';
+DB::$dbName = 'db';
+DB::$host = 'host';
+DB::$encoding = 'encoding';
+
+?>
